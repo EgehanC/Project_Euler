4a7c7ea0dd9c2baa98d7338a927894c525e6199b
Egehan Ceylan <cegehan.co@gmail.com>
2022-11-18 15:23:35 +0300

    Update main.py

--- 17/main.py
+++ 17/main.py
@@ -1,1 +1,28 @@
 ### Problem 17
+import inflect
+
+p = inflect.engine()
+
+
+def count_digits(a, b):
+    total_count = 0
+    for i in range(a, b + 1):
+        total_count += digit_to_word(i)
+    return total_count
+
+
+def digit_to_word(j):
+    word = list(p.number_to_words(j))
+    count = 0
+    for k in word:
+        if k not in [',', '-', ' ']:
+            count += 1
+    return count
+
+
+def main():
+    sum = count_digits(1, 1000)
+    print(sum)
+
+
+main()
