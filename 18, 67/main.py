5c75eda4b174de6d4a6ab333fc93d69339d345b4
Egehan Ceylan <cegehan.co@gmail.com>
2022-11-21 18:59:20 +0300

    Update main.py

--- 18, 67/main.py
+++ 18, 67/main.py
@@ -1,10 +1,10 @@
 ### Problem 18
 import time
 
-start = time.time()
-
 
 def main():
+    start = time.time()
+
     with open('18.txt') as f:
         # All the numbers in the file
         number = f.read()
