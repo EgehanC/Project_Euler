50637787efb69c89c99fd5eabe020f8a42512ec0
Egehan Ceylan <cegehan.co@gmail.com>
2022-11-24 15:21:21 +0300

    Update main.py

--- 26/main.py
+++ 26/main.py
@@ -1,13 +1,11 @@
 def recurring_cycle(n):
-    print(n)
-    n = str(n)[2:]
-    print(n)
-    cycle = []
-    for i in n:
-        if i not in cycle:
-            cycle.append(i)
 
-    return len(cycle)
+    remainders = []
+    for i in n:
+        if i not in remainders:
+            remainders.append(i)
+
+    return len(remainders)
 
 
 def main():
@@ -15,7 +13,7 @@ def main():
     num = 0
     for d in range(2, 1001):
         print(f"Current d: {d}")
-        current_cycle = recurring_cycle(round(1/d, 30))
+        current_cycle = recurring_cycle(d)
         if current_cycle > max_cycle:
             max_cycle = current_cycle
             num = d
